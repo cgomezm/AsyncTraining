b7ebce1b94c0f9440973e26f036f8d5125c1396c
Camilo Andres Gomez <camiloa205@gmail.com>
2020-04-06 15:09:36 -0500

    Add project files.

---
+++ FirstExercise/FileInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstExercise
+{
+    public class FileInfo
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ FirstExercise/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace FirstExercise
+{
+    class Program
+    {
+        static string filePath = @"C:\AsyncTraining";
+        static List<FileInfo> filesData = new List<FileInfo>();
+            
+        public static void Main(string[] args)
+        {
+            MonitorDirectory(filePath);
+            Console.ReadKey();
+        }
+
+        private static void MonitorDirectory(string path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+
+            watcher.Path = filePath;
+            watcher.NotifyFilter = NotifyFilters.CreationTime
+                                | NotifyFilters.FileName;
+
+            watcher.Filter = "*.*";
+            watcher.Created += OnFileCreated;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private static void OnFileCreated(object source, FileSystemEventArgs e)
+        {            
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                ThreadPool.GetMinThreads(out int wt, out int cp);
+                ThreadPool.SetMinThreads(4, cp);
+                ThreadPool.SetMaxThreads(4, cp);
+                
+                lock (filesData)
+                {
+                    filesData.Add(new FileInfo { FileName = e.Name, Content = File.ReadAllText(e.FullPath) });
+                    if (filesData.Count == 10)
+                    {
+                        filesData.ForEach(f =>
+                        {
+                            Console.WriteLine($"File: {f.FileName}, contains data: {f.Content}");
+                        });
+
+                        filesData.Clear();
+                    }
+                }                
+            });
+        }
+    }
+}
